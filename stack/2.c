b5b540b3e47cd31e7e80103639f6a9926500942c
liushengxi <liushengxi@localhost.localdomain>
2017-10-25 23:44:52 +0800

    test

--- stack/2.c
+++ stack/2.c
@@ -7,7 +7,7 @@
 
 #include<stdio.h>
 #define MAX  1000
-int level(char p)  //规定运算符优先级
+int level(char p)    //得到运算符的优先级顺序
 {
     int temp ;
     switch(p)
@@ -22,34 +22,34 @@ int level(char p)  //规定运算符优先级
     return temp ;
 }
 
-    
 int cal(int number[] ,int *numberTop ,char Symbol[] ,int *SymbolTop)
 {
-    char operation = Symbol[*SymbolTop--]; //出符号
-    int value1=number[*numberTop--];
+    char operation = Symbol[*SymbolTop--]; //出符号，必进行计算
+    int value1=number[*numberTop--];  //出两个数进行计算
     int value2=number[*numberTop--];
     int temp;
     switch(operation)
     {
-        case '+':temp=value2+value1;  break;
+        case '+':temp=value2+value1;  break; //顺序一定是values2 '/' 或者 '-' values1
         case '-':temp=value2 -value1; break;
         case '*':temp =value2*value1; break;
         case '/':temp =value2/value1; break;
     }
-    return temp;
+//    *numberTop++;
+  //  number[*numberTop] = temp ;  //把结果再压进数值栈中
+  return temp ;
 }
+
 int fun(char str[])
 {
     char  Symbol[MAX];
-    int SymbolTop= -1;  //运算符栈
-    int numberTop= -1;  //数值栈
+    int SymbolTop= -1;   //运算符栈
+    int numberTop= -1;   //数值栈
     int  number[MAX]; 
-    printf("str == %s \n",str); 
-
     int  y = 0; //用来计算多位数 
     int i= 0 ;
+    int temp;
     Symbol[++SymbolTop]='@'; //先把 @ 入到符号栈中,就不用判断栈是否空了！！！这是一个比较聪明的想法
-
     while(str[i]){  //先遍历该字符串
         y= 0 ;
         if(str[i] <= '9' && str[i] >= '0'){ //是数字
@@ -57,22 +57,13 @@ int fun(char str[])
                 y= y*10+str[i]-'0' ;
                     i++;
             }
-            printf("y== %d \n",y);
             number[++numberTop]=y ; //入栈数值
-            printf("number[numberTop== %d \n",number[numberTop]);
         } 
-        else if(str[i] > '9' ||  str[i] < '0' ){   //不是数字
-
-            printf("levelstr[i]  == %d \n",level(str[i]));
-            printf("Symbol[SymbolTop == %c\n" ,Symbol[SymbolTop]);
-            printf("levelstr[i]  == %d \n",level(Symbol[SymbolTop]));
-
-            while(level(str[i]) <= level(Symbol[SymbolTop]))  //让栈中比它大的和等于它的都出栈！！！中缀表达式转后缀表达式的核心
-                                                            //从符号栈中出一个符号，从数值栈中出两个数字，计算后压入数值栈
-            {
-                printf("kebiorembemobmomborebmo\n");
-                number[++numberTop] = cal(number,&numberTop,Symbol,&SymbolTop);
-                printf("number[numberTop== %d \n",number[numberTop]);
+        else if((str[i] > '9' ||  str[i] < '0') && str[i] != '(' && str[i] != ')' ){   //不是数字,排除左,右括号的情况！！！
+            while(level(str[i]) <= level(Symbol[SymbolTop])){  //让栈中比它大的和等于它的都出栈！！！中缀表达式转后缀表达式的核心
+            //从符号栈中出一个符号，从数值栈中出两个数字，计算后压入数值栈
+                    temp=cal(number,&numberTop,Symbol,&SymbolTop);
+                    number[++numberTop]=temp;
             }
             Symbol[++SymbolTop] = str[i];
             i++;
@@ -83,16 +74,21 @@ int fun(char str[])
         }
         else if(str[i] == ')'){   //进行运算直到遇到左括号
             while(Symbol[SymbolTop] != '('){
-                number[++numberTop] =cal(number,&numberTop,Symbol,&SymbolTop);
+                    temp=cal(number,&numberTop,Symbol,&SymbolTop);
+                    number[++numberTop]=temp;
             }
             SymbolTop--;
             i++;
         }
-    // 将Symbol 栈检查一下，返回number 的栈顶，结束
+    //i++;
+    //将Symbol 栈检查一下，返回number 的栈顶，结束
+    }
+    while(Symbol[SymbolTop] != '@'){
+        temp=cal(number,&numberTop,Symbol,&SymbolTop);
+        number[++numberTop]=temp;
     }
     return number[numberTop];
 }
-
 int main(void)
 {
     char str[MAX];
--- stack/3.c
+++ stack/3.c
@@ -21,35 +21,15 @@ int level(char p)  //规定运算符优先级
     }
     return temp ;
 }
-
-    
-int cal(int number[] ,int *numberTop ,char Symbol[] ,int *SymbolTop)
-{
-    char operation = Symbol[*SymbolTop--]; //出符号
-    int value1=number[*numberTop--];
-    int value2=number[*numberTop--];
-    int temp;
-    switch(operation)
-    {
-        case '+':temp=value2+value1;  break;
-        case '-':temp=value2 -value1; break;
-        case '*':temp =value2*value1; break;
-        case '/':temp =value2/value1; break;
-    }
-    return temp;
-}
 int fun(char str[])
 {
     char  Symbol[MAX];
     int SymbolTop= -1;  //运算符栈
     int numberTop= -1;  //数值栈
     int  number[MAX]; 
-    printf("str == %s \n",str); 
-
     int  y = 0; //用来计算多位数 
     int i= 0 ;
     Symbol[++SymbolTop]='@'; //先把 @ 入到符号栈中,就不用判断栈是否空了！！！这是一个比较聪明的想法
-
     while(str[i]){  //先遍历该字符串
         y= 0 ;
         if(str[i] <= '9' && str[i] >= '0'){ //是数字
@@ -57,22 +37,24 @@ int fun(char str[])
                 y= y*10+str[i]-'0' ;
                     i++;
             }
-            printf("y== %d \n",y);
             number[++numberTop]=y ; //入栈数值
-            printf("number[numberTop== %d \n",number[numberTop]);
         } 
-        else if(str[i] > '9' ||  str[i] < '0' ){   //不是数字
-
-            printf("levelstr[i]  == %d \n",level(str[i]));
-            printf("Symbol[SymbolTop == %c\n" ,Symbol[SymbolTop]);
-            printf("levelstr[i]  == %d \n",level(Symbol[SymbolTop]));
-
+        else if((str[i] > '9' ||  str[i] < '0') && str[i] != '(' && str[i] != ')' ){   //不是数字,排除左,右括号的情况
             while(level(str[i]) <= level(Symbol[SymbolTop]))  //让栈中比它大的和等于它的都出栈！！！中缀表达式转后缀表达式的核心
                                                             //从符号栈中出一个符号，从数值栈中出两个数字，计算后压入数值栈
             {
-                printf("kebiorembemobmomborebmo\n");
-                number[++numberTop] = cal(number,&numberTop,Symbol,&SymbolTop);
-                printf("number[numberTop== %d \n",number[numberTop]);
+                char operation = Symbol[SymbolTop--]; //出符号
+                int value1=number[numberTop--];
+                int value2=number[numberTop--];
+                int temp;
+                switch(operation)
+                {
+                    case '+':temp=value2+value1;  break;
+                    case '-':temp=value2 -value1; break;
+                    case '*':temp =value2*value1; break;
+                    case '/':temp =value2/value1; break;
+                }
+                number[++numberTop] = temp ;
             }
             Symbol[++SymbolTop] = str[i];
             i++;
@@ -83,16 +65,40 @@ int fun(char str[])
         }
         else if(str[i] == ')'){   //进行运算直到遇到左括号
             while(Symbol[SymbolTop] != '('){
-                number[++numberTop] =cal(number,&numberTop,Symbol,&SymbolTop);
+                char operation = Symbol[SymbolTop--]; //出符号
+                int value1=number[numberTop--];
+                int value2=number[numberTop--];
+                int temp;
+                switch(operation)
+                {
+                    case '+':temp=value2+value1;  break;
+                    case '-':temp=value2 -value1; break;
+                    case '*':temp =value2*value1; break;
+                    case '/':temp =value2/value1; break; //一定是value2  '/'或'-' value1 ,顺序不能变
+                }
+                number[++numberTop] = temp ;
             }
             SymbolTop--;
             i++;
         }
     // 将Symbol 栈检查一下，返回number 的栈顶，结束
     }
+   while(Symbol[SymbolTop] != '@'){
+        char operation = Symbol[SymbolTop--]; //出符号
+        int value1=number[numberTop--];
+        int value2=number[numberTop--];
+        int temp;
+        switch(operation)
+        {
+            case '+':temp=value2+value1;  break;
+            case '-':temp=value2 -value1; break;
+            case '*':temp =value2*value1; break;
+            case '/':temp =value2/value1; break;
+        }
+        number[++numberTop] = temp ;
+   }
     return number[numberTop];
 }
-
 int main(void)
 {
     char str[MAX];
