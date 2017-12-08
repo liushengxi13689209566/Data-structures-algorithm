1364eb6fc522fb9ffe8f21f14d77380be25574f5
liushengxi <liushengxi@localhost.localdomain>
2017-12-08 17:40:54 +0800

    D

---
+++ graph/邻接表/Map.cpp
@@ -0,0 +1,39 @@
+/*************************************************************************
+	> File Name: Map.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年12月08日 星期五 12时55分05秒
+ ************************************************************************/
+
+#include<iostream>
+#include"Map.h"
+using namespace std;
+Map::Map(int temp) //构造函数
+{
+    MAXNODESIZE = temp ;
+    vertexNode = new vertex[MAXNODESIZE]; //顶点数组
+    vertexCount = 0 ;
+    arcCount = 0;
+}
+Map::~Map() 
+{
+    delete []vertexNode;
+}
+bool Map::addVertexToVertexNode(char ch)
+{
+    vertexNode[vertexCount].vertexData  =  ch ;
+    vertexCount++;
+    return true ;
+}
+
+bool Map::setArc(char Node,int index,int weight)//加边
+{
+    Edge *p = NULL ;
+    for(int i=0;i< MAXNODESIZE;++i){
+        if(vertexNode[i].vertexData == Node){
+            p=vertexNode[i].pEdge  ;
+
+        }
+    }
+}
+
---
+++ graph/邻接表/Map.h
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: Map.h
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年12月08日 星期五 12时24分47秒
+ ************************************************************************/
+
+#ifndef _MAP_H
+#define _MAP_H
+class vertex{  //顶点
+    public:
+    vertex(char ch= 0 );
+
+    char vertexData ; //顶点数据
+    bool visted ;  //顶点是否被访问
+    Edge *pEdge;   //连接边
+};
+class Edge{   //边
+    public:
+    Edge();
+    private:
+    int Nodeindex ; //下一条边所链接的顶点的下标
+    int weight;     //权重
+    Edge *next ;    //链表指针
+};
+
+class Map{    //图
+    public:
+    Map(int temp);
+    ~Map();
+    bool addVertexToVertexNode(char ch) ;
+    bool setArc(char Node,int index,int weight);//加入边
+    void print();
+
+    private：
+    int MAXNODESIZE ; //最大顶点数
+    vertex *vertexNode; //顶点数组
+    int vertexCount ; //顶点个数
+    int arcCount ;  //边的个数
+};
+
+#endif
