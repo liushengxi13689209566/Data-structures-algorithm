cf2d339772bd3fc22f5c24fee7e84203771f914d
liushengxi13689209566 <liushengxi13689209566@163.com>
2018-12-09 23:25:46 +0800

    写了一下链表的逆置，但自我感觉代码很丑

---
+++ Linear_table/reverse_list.cpp
@@ -0,0 +1,85 @@
+/*************************************************************************
+	> File Name: reverse_list.cpp
+	> Author: Liu Shengxi 
+	> Mail: 13689209566@163.com
+	> Created Time: 2018年12月09日 星期日 21时51分31秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+template <typename T>
+struct Node
+{
+    Node(T t)
+    {
+        data = t;
+    }
+    T data;
+    Node *next;
+};
+
+template <typename T>
+class List
+{
+  public:
+    List()
+    {
+        CreatList();
+    }
+    void CreatList()
+    {
+        head = new Node<T>(-100);
+        Node<T> *temp = nullptr;
+        rear = head;
+        for (int i = 0; i < 10; i++)
+        {
+            temp = new Node<T>(i);
+            temp->next = nullptr;
+            rear->next = temp;
+            rear = temp;
+        }
+        rear->next = nullptr;
+    }
+    void ReverseList()
+    {
+        Node<T> *curr, *beh;
+        curr = head->next;
+        rear = head->next;
+        beh = curr->next;
+
+        while (beh)
+        {
+            curr->next = head->next;
+            head->next = curr;
+
+            curr = beh;
+            beh = beh->next;
+        }
+        curr->next = head->next;
+        head->next = curr;
+        rear->next = nullptr;
+    }
+    void Print()
+    {
+        Node<T> *temp = head->next;
+        while (temp)
+        {
+            std::cout << temp->data << "  ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+
+  private:
+    Node<T> *head;
+    Node<T> *rear;
+};
+
+int main(void)
+{
+    List<int> list;
+    list.Print();
+
+    list.ReverseList();
+    list.Print();
+}
