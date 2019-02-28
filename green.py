177775da7e74d4b85e6361c2155b1a15d05735d8
liushengxi13689209566 <liushengxi13689209566@gmail.com>
2019-03-01 00:14:31 +0800

    update

--- green.py
+++ green.py
@@ -37,4 +37,4 @@ def daily_commit(start_date, end_date):
 
 
 if __name__ == '__main__':
-    daily_commit(datetime.date(2018, 3, 1), datetime.date(2019, 3, 1))
+    daily_commit(datetime.date(2018, 3, 1), datetime.date(2018, 4, 1))
